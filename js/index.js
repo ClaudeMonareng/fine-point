dddfc1ecfafe1f1c690e59ae0e6c22d7a1c8c325
Lehlohonolo Monareng <lehlohonolomonareng@yahoo.com>
2024-09-04 09:30:49 +0200

    Latest Changes

--- js/index.js
+++ js/index.js
@@ -1,36 +1,38 @@
 
-const menuToggle= document.querySelector('.menuToggle');
-const navigation= document.querySelector('.navigation');
+const menuToggle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
 
-menuToggle.onclick = function(){
-    menuToggle.classList.toggle('active');
-    navigation.classList.toggle('active');
+menuToggle.onclick = function () {
+  menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
 }
-window.addEventListener('scroll', function(){
-    const header= document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);
 });
 
 //now, we will fix navigation issue...menu close after clicked any link
-function toggleMenu(){
-    menuToggle.classList.remove('active');
-    navigation.classList.remove('active');
+
+function toggleMenu() {
+  menuToggle.classList.remove('active');
+  navigation.classList.remove('active');
 }
 
 // Initialize Swiper 
-  var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  })
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
